6d5b86d8dcb22470d5d3dfd7126c3a68e27bdee7
Christine Augustine <blacchoy@gmail.com>
2020-05-04 12:31:17 -0700

    add async function to main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,26 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
 import $ from "jquery";
+import { CurrencyExchanger } from "./exchanger";
+
+$(document).ready(function() {
+  $('#exchangeRate').submit(function() {
+    event.preventDefault();
+    let userUSD = $('#userAmt').val();
+    let convert = $('#convertAmt').val();
+
+(async () => {
+  let exchangeService = new exchangeService();
+  const response = await exchangeService.getExchangeRate();
+  getElements(response);
+  console.log(response);
+})();
+
+// function getElements (response) {
+//   if (response) {
+//     )
+//   } else {
+
+//   }
+// }
+
+});
+});
