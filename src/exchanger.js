e352bd5da0ae749bba1eb2a67ebc658c4d39bf6a
Christine Augustine <blacchoy@gmail.com>
2020-05-04 08:00:21 -0700

    set up API call for exchanger

---
+++ src/exchanger.js
@@ -0,0 +1,16 @@
+export class WeatherService {
+  async getWeatherByCity(city) {
+    try {
+      let response = await fetch(`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}`);
+      let jsonifiedResponse;
+      if (response.ok && response.status == 200) {
+        jsonifiedResponse = await response.json();
+      } else {
+        jsonifiedResponse = false;
+      }
+      return jsonifiedResponse;
+    } catch {
+      return false;
+    }
+  }
+}
