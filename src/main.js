0871e0a93d5fb2355bcd32d25681719c4a3726cc
blacchoy@gmail.com <blacchoy@gmail.com>
2020-05-05 18:07:34 -0700

    add remaining currencies to conversion function & remove console logs

--- src/main.js
+++ src/main.js
@@ -7,21 +7,28 @@ $(document).ready(function() {
   $('#form').submit(function(event) {
     event.preventDefault();
     const amount = $("#usd").val();
-    console.log(amount);
     const convertCurrency = $("#convert").val();
 
     (async () => {
-      console.log("Made it here.");
       let currencyExchanger = new CurrencyExchanger();
       const response = await currencyExchanger.getCurrency();
       getConversion(response);
-      console.log(response);
     })();
 
     function getConversion(response) {
-      if (response) {
+      if (convertCurrency === "AED") {
         $('#output').text(`Your USD ${amount} is ${response.conversion_rates.AED}`);
-      } else {
+       } else if (convertCurrency === "ARS") {
+          $('#output').text(`Your USD ${amount} is ${response.conversion_rates.ARS}`);
+        } else if (convertCurrency === "AUD") {
+          $('#output').text(`Your USD ${amount} is ${response.conversion_rates.AUD}`);
+        } else if (convertCurrency === "BGN") {
+          $('#output').text(`Your USD ${amount} is ${response.conversion_rates.BGN}`);
+        } else if (convertCurrency === "BRL") {
+          $('#output').text(`Your USD ${amount} is ${response.conversion_rates.BRL}`);
+        } else if (convertCurrency === "BSD") {
+          $('#output').text(`Your USD ${amount} is ${response.conversion_rates.BSD}`);
+      } else (convertCurrency === "NaN") {
         $('#output').text(`There was an error handling your request.`);
       }
    }
